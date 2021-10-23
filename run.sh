2bd9491218a42eace65cb737b8b30ce7d915a8ca
Senal Bulumulle <senal.bulumulle@gmail.com>
2021-10-22 21:09:07 -0400

    Fixed some errors after porting from AIRO-Chatbot
    https://github.com/senalbulumulle/AIRO-Chatbot/

---
+++ run.sh
@@ -0,0 +1,9 @@
+###########################################################
+#			run.sh
+#	A luxurious script that would essentially run a program
+#	Script Written by Senal Bulumulle
+#
+#
+##########################################################
+
+electron index.js
